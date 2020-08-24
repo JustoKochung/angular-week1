868d809478a3e1efb1e4915aee816f45dabf6e7d
JustoKochung <kochungjusto26@gmail.com>
2020-08-24 11:55:06 +0300

    add emitting events to child components

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,14 @@
 export class Quote {
   showDescription: boolean;
-  constructor(public id: number, public name: string, public description: string) {
+  constructor(
+    public id: number,
+    public quoteName: string,
+    public author: string,
+    public creator: string,
+    public dateCreated: Date,
+    public dislike: number,
+    public like: number,
+  ) {
     this.showDescription = false;
   }
 }
