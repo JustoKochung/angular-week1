65cb777c620775632404eb5570da22dfb3508cb0
JustoKochung <kochungjusto26@gmail.com>
2020-08-24 15:51:44 +0300

    modify dislike/like component

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,9 +8,9 @@ import { Quote } from "../quote";
 export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
     new Quote(1, '\"Kaffirs are as a rule uncivilised—the convicts even more so. They are troublesome, very dirty and live almost like animals\"', 'Mahatma Gandhi', 'Justo', new Date(2020, 10, 4), 0, 0),
-    new Quote(2, '\"Never trust a Jew!\"', 'Nikola Tesla', 'Kelvin', new Date(2021, 10, 6), 0, 0),
+    new Quote(2, '\"Never trust a Jew!\"', 'Nikola Tesla', 'Kelvin', new Date(2021, 10, 6), 3, 8),
     new Quote(3, '\"When a woman has scholarly inclinations there is usually something wrong with her sexual organs.\"', 'Friedrich Nietzsche', 'Domnick', new Date(2018, 10, 5), 0, 0),
-    new Quote(4, '\“Educating a beautiful woman is like pouring honey into a fine Swiss watch: everything stops.\”', 'Kurt Vonnegut', 'Mercy', new Date(2020, 7, 8), 0, 0),
+    new Quote(4, '\“Educating a beautiful woman is like pouring honey into a fine Swiss watch: everything stops.\”', 'Kurt Vonnegut', 'Mercy', new Date(2020, 7, 8), 4, 6),
   ];
   toggleDetails(index) {
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
