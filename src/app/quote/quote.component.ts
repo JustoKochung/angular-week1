f3750bf36e78dd5453ff164df8c4b3e14fb55092
JustoKochung <kochungjusto26@gmail.com>
2020-08-24 12:47:14 +0300

    add pipe for deleting quotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -15,9 +15,17 @@ export class QuoteComponent implements OnInit {
   toggleDetails(index) {
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
   }
-  completeQuote(isComplete, index) {
+  deleteQuote(isComplete, index) {
     if (isComplete) {
       this.quotes.splice(index, 1);
+    }
+  }
+  deleteQuote(isComplete, index) {
+    if (isComplete) {
+      let toDelete = confirm(`Are you sure of deleting this quote?`)
+      if (toDelete) {
+        this.quotes.splice(index, 1)
+      }
     }
   }
   constructor() { }
